7eef7f28b78880f8cc5c1ce569d8ce7dbaf6693d
nenadveljkovic <nenad.veljkovic98@gmail.com>
2021-04-12 14:15:56 +0200

    Lako do stana - početna verzija

---
+++ Aplikacija/LakoDoStana/LakoDoStana/Models/LDSDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LakoDoStana.Models
+{
+    public class LDSDatabaseSettings:ILDSDatabaseSettings
+    {
+        public string LDSCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface ILDSDatabaseSettings
+    {
+        string LDSCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
